4b8017c081680df657c6f64a722a7efda334c1b2
swadhi03 <swathiksanthosh03@gmail.com>
2024-06-07 23:51:19 +0530

    navigation completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import AddTodo from './components/AddTodo';
+import SearchTodo from './components/SearchTodo';
+import DeleteTodo from './components/DeleteTodo';
+import ViewAll from './components/ViewAll';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<AddTodo/>}/>
+        <Route path='search/' element={<SearchTodo/>}/>
+        <Route path='delete/' element={<DeleteTodo/>}/>
+        <Route path='view/' element={<ViewAll/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/SearchTodo.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const SearchTodo = () => {
+  return (
+    <div>
+        <div className="container">
+            <Navbar/>
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row g-3">
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Title</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <button className="btn btn-success">Search</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default SearchTodo
